64d3dba0d61e532970ea68f35cff45f21399ec36
Esysteme <aurelien.lequoy@gmail.com>
2014-03-11 18:52:29 +0100

    Update header.view.php

--- application/view/Layout/header.view.php
+++ application/view/Layout/header.view.php
@@ -1,42 +1,23 @@
  <?php
 
-<<<<<<< HEAD
-  use \Glial\I18n\I18n;
+use \Glial\I18n\I18n;
 
-  echo "<!DOCTYPE html>\n";
-   echo "<html lang=\"" . I18n::Get() . "\">";
-   echo "<head>\n";
-    echo "<meta charset=utf-8 />\n";
-    echo "<meta name=\"Keywords\" content=\"\" />\n";
-     echo "<meta name=\"Description\" content=\"\" />\n";
-     echo "<meta name=\"Author\" content=\"Aurelien LEQUOY\" />\n";
-      echo "<meta name=\"robots\" content=\"index,follow,all\" />\n";
-      echo "<meta name=\"generator\" content=\"GLIALE 1.1\" />\n";
-       echo "<meta name=\"runtime\" content=\"[PAGE_GENERATION]\" />\n";
-       echo "<link rel=\"shortcut icon\" href=\"favicon.ico\" />";
-        echo "<title>" . $GLIALE_TITLE . " - Glial 2.1.2</title>\n";
-        echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"" . CSS . "default.style.css\" />\n";
+echo "<!DOCTYPE html>\n";
+echo "<html lang=\"" . I18n::Get() . "\">";
+echo "<head>\n";
+echo "<meta charset=utf-8 />\n";
+echo "<meta name=\"Keywords\" content=\"\" />\n";
+echo "<meta name=\"Description\" content=\"\" />\n";
+echo "<meta name=\"Author\" content=\"Aurelien LEQUOY\" />\n";
+echo "<meta name=\"robots\" content=\"index,follow,all\" />\n";
+echo "<meta name=\"generator\" content=\"GLIALE 1.1\" />\n";
+echo "<meta name=\"runtime\" content=\"[PAGE_GENERATION]\" />\n";
+echo "<link rel=\"shortcut icon\" href=\"favicon.ico\" />";
+echo "<title>" . $GLIALE_TITLE . " - Glial 2.1.2</title>\n";
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"" . CSS . "default.style.css\" />\n";
 
-         ?>
-=======
- use \Glial\I18n\I18n;
+?>
 
- echo "<!DOCTYPE html>\n";
- echo "<html lang=\"" . I18n::Get() . "\">";
- echo "<head>\n";
- echo "<meta charset=utf-8 />\n";
- echo "<meta name=\"Keywords\" content=\"\" />\n";
- echo "<meta name=\"Description\" content=\"\" />\n";
- echo "<meta name=\"Author\" content=\"Aurelien LEQUOY\" />\n";
- echo "<meta name=\"robots\" content=\"index,follow,all\" />\n";
- echo "<meta name=\"generator\" content=\"GLIALE 1.1\" />\n";
- echo "<meta name=\"runtime\" content=\"[PAGE_GENERATION]\" />\n";
- echo "<link rel=\"shortcut icon\" href=\"favicon.ico\" />";
- echo "<title>" . $GLIALE_TITLE . " - Glial 2.1.2</title>\n";
- echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"" . CSS . "default.style.css\" />\n";
-
- ?>
->>>>>>> 575a942482d149e63a05e3bb10fecb8673d6ca3b
  </head>
  <body>
 
